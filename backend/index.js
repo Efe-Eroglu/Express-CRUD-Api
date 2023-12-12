d9269b893d25b59416319d41d6ca9c627e6ea504
Efe <eferoglu1967@gmail.com>
2023-12-12 14:46:24 +0300

    Get  dummy data

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersRouter from "./routes/users.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use("/users",usersRouter);
 app.use("*",(req,res)=>{
     res.status(404).send("Page Not Found 404")
--- frontend/crudApi/src/pages/home/Home.jsx
+++ frontend/crudApi/src/pages/home/Home.jsx
@@ -1,38 +1,56 @@
-import "./home.css"
+import { useEffect, useState } from "react";
+import "./home.css";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  const getUsers = async () => {
+    const res = await axios.get("http://localhost:5000/users");
+
+    if (res.status === 200) {
+      setData(res.data);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="table-wrapper">
-        <table>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Country</th>
-                    <th>Contact</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Efe</td>
-                    <td>efe@example.com</td>
-                    <td>Turkey</td>
-                    <td>+905361</td>
-                    <td>
-                        <div className="buttons">
-                            <button className="btn btn-primary">View</button>
-                            <button className="btn btn-success">Edit</button>
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Country</th>
+            <th>Contact</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data && data.map((user,index) => (
+              <tr key={user.id}>
+                <td>{index+1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.country}</td>
+                <td>{user.contact}</td>
+                <td>
+                  <div className="buttons">
+                    <button className="btn btn-primary">View</button>
+                    <button className="btn btn-success">Edit</button>
+                    <button className="btn btn-danger">Delete</button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
